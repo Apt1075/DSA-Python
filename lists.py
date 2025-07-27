42702caa5e630a2ae0688b9150857c29fd7cb31f
Arpit kumar <arpit.kumar.101196@gmail.com>
2025-07-27 12:53:04 +0530

    add remove sec

--- lists.py
+++ lists.py
@@ -5,3 +5,11 @@ print(demo)
 demo[4] = "vikash"
 
 print("updated lists = ",demo)
+
+
+del demo[0] #del keywords specfic for keys
+print("after deletion lists = ", demo)
+
+
+demo.remove("vikash") #remove keywords for value if exits
+print("after remove the lists = ", demo)
