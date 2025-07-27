9a63d3e6bdecef6ee996f39e2534a755deb08b4b
Arpit kumar <arpit.kumar.101196@gmail.com>
2025-07-27 18:21:45 +0530

    dict

---
+++ mydict.py
@@ -0,0 +1,17 @@
+mydictt = {"arpit":1,"sanjay":2,"arjun":3,"ankit":4,"raja":5}
+# print(mydictt)
+
+# mydictt = dict({"arpit":1,"sanjay":2,"arjun":3,"ankit":4,"raja":5})
+# print(mydictt)
+
+
+# print(mydictt["sanjay"])
+
+
+print(mydictt.get("sanjay"))
+
+print(mydictt.keys())
+
+print(mydictt.values())
+
+print(mydictt.items())
