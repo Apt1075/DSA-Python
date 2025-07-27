8e6bb2c37e9baa7fd629092121821489859addb3
Arpit kumar <arpit.kumar.101196@gmail.com>
2025-07-27 18:32:20 +0530

    breakandcontinue

---
+++ breakandcontinue.py
@@ -0,0 +1,16 @@
+string = "Arpit Kumar"
+
+Value = "a"
+
+# for i in string:
+#     # print(i)
+#     if i in Value:
+#         print(i)
+#         break
+
+
+for i in string:
+    # print(i)
+    if i != Value:
+        print(i)
+        continue
