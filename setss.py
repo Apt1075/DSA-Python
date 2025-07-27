2d2625b9fb43a9c75f092ffc5a6a343f0f915a63
Arpit kumar <arpit.kumar.101196@gmail.com>
2025-07-27 17:31:26 +0530

    sets

---
+++ setss.py
@@ -0,0 +1,37 @@
+mysets = {"arpit","sanjay","arjun","ankit","raja"}
+print(mysets)
+
+# mysets.add("vikash")
+# print(mysets)
+# mysets.remove("vikash")
+# print(mysets)
+# mysets.discard("vikash")
+# print(mysets)
+
+
+mysets1 = {"arpit",1,2,1.5,2.5}
+print(mysets1)
+
+# print("len = ",len(mysets1))
+
+
+# for x in mysets1:
+#     print(x)
+
+
+
+# mysets.update(mysets1)
+# print(mysets)
+
+# mysets.clear()
+# print(mysets)
+
+# del mysets
+# print(mysets)
+
+res = mysets1.difference(mysets)
+print("response = ",res)
+
+# mysets.difference_update(mysets1)
+# print(mysets)
+
